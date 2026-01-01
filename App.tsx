b77d5b4ab237d058ed1fcf853d494be9d1eaed32
KuzikevichDavid <wliorjroo@gmail.com>
2026-01-01 10:19:24 +0300

    feat: draft rn config

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { NAV_THEME } from '@/lib/theme';
+import { ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const { colorScheme } = useColorScheme();
+  
   return (
-    <>
+    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <PortalHost />
+    </ThemeProvider>
   );
 }
